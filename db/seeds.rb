d53740ec45ce7b2fad2287b917b2b94712aecb48
Daniel Polo <od.polo@gmail.com>
2020-08-14 10:21:49 -0500

    Added photo yo Article model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+
+file = URI.open('https://images.unsplash.com/photo-1597389463089-ce511f6cb229?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2700&q=80')
+article = Article.new(title: 'NES', body: 'A great console')
+article.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+article.save
